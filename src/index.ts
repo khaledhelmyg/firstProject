0bc6d8698e7b8ca3c2ddffa1fb6c8eed6a794774
Khalid helmy <khaledhelmyabdo12345@yahoo.com>
2022-03-23 13:10:39 +0200

    seconed commit complet the projec the project

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
-
+import router from './routes';
+import File from './file';
 const app: express.Application = express();
-const PORT = 300;
+const PORT = 5000;
+
+app.use(router);
 
 app.get('/', (req, res) => {
   res.send('helow world');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async (): Promise<void> => {
+  await File.createThumbPath();
+
+  const url: string = `\x1b[2mhttp://localhost:${PORT}\x1b[0m`;
+  console.log(`Please open ${url} to review the project ...`);
   console.log(`srever is runung at http://localhost:${PORT}`);
 });
+export default app;
